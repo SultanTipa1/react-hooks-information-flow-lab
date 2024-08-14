324ef3ff724c2fc13f7f58dc0aea61fb8c5ff735
SultanTipa1 <josephngumbaugush@gmail.com>
2024-08-14 15:23:03 +0300

    Adds all files

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,30 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function onDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+  function handleDarkModeClick() {
+    setIsDarkMode((prevMode) => !prevMode);
   }
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
+    <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+    <ShoppingList 
+      items={itemData} 
+      selectedCategory={selectedCategory} 
+      onCategoryChange={handleCategoryChange} 
+    />
+  </div>
   );
 }
 
